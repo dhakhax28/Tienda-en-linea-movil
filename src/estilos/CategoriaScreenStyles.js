991ae7503ef7bc6fba2e6c53ff35e5ce41ac5efd
Fuentes <20220406@ricaldone.edu.sv>
2024-06-27 22:04:52 -0600

    Tamaños

--- src/estilos/CategoriaScreenStyles.js
+++ src/estilos/CategoriaScreenStyles.js
@@ -6,12 +6,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#f5f5f5', // light grey background color for a modern look
-    paddingVertical: 65,
+    paddingVertical: 20,
   },
   title: {
-    fontSize: 30,
+    fontSize: 26,
     fontWeight: 'bold',
-    marginBottom: 30,
+    marginBottom: 20,
     color: '#333', // dark grey color for the title
   },
   sectionTitle: {
@@ -25,15 +25,15 @@ const styles = StyleSheet.create({
     width: '90%',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    justifyContent: 'space-between', // Space between the cards
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
   card: {
-    width: '100%', // Adjust the width to fit two cards in a row with some margin
+    width: '99%',
     backgroundColor: '#fff',
-    borderRadius: 18,
-    padding: 20,
-    margin: 10, // Add margin for spacing between cards
+    borderRadius: 20,
+    padding: 33,
+    marginBottom: 20,
     alignItems: 'center',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 5 },
@@ -42,19 +42,19 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   image: {
-    width: 40,
-    height: 40,
+    width: 75,
+    height: 75,
   },
   cardTitle: {
-    fontSize: 18,
+    fontSize: 15,
     fontWeight: 'bold',
-    marginTop: 10,
+    marginTop: 15,
     textAlign: 'center',
     color: '#333',
   },
   logoutButton: {
     position: 'absolute',
-    top: 40,
+    top: 50,
     right: 20,
   },
 });
